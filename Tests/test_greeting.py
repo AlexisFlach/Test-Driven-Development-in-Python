40c5d1bf13da6f328232be69246a512390a6d9c7
alexisflach <alexis.flach87@gmail.com>
2021-09-06 13:22:31 +0200

    made test pass

--- Tests/test_greeting.py
+++ Tests/test_greeting.py
@@ -4,7 +4,7 @@ import unittest
 class TestGreeting(unittest.TestCase):
   def test_greeting(self):
     msg = greeting("Alex")
-    self.assertEqual(msg, "hello Alex")
+    self.assertEqual(msg, "Greetings Alex")
 
 if __name__ == '__main__':
 	unittest.main()
