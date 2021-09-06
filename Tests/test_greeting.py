65f7dd591c6b363110d4414a30ef628c43c2428d
alexisflach <alexis.flach87@gmail.com>
2021-09-06 10:49:06 +0200

    add failed test

--- Tests/test_greeting.py
+++ Tests/test_greeting.py
@@ -4,7 +4,7 @@ import unittest
 class TestGreeting(unittest.TestCase):
   def test_greeting(self):
     msg = greeting("Alex")
-    self.assertEqual(msg, "Greetings Alex")
+    self.assertEqual(msg, "Hello Alex")
 
 if __name__ == '__main__':
 	unittest.main()
